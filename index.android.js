744503467c8e183f4e2a2a582857e56845d33cdd
Vishrut Reddi <vishrutreddi@gmail.com>
2017-04-13 00:55:30 -0700

    Init react-native project

---
+++ index.android.js
@@ -0,0 +1,1 @@
+// Android, where you at?
---
+++ index.ios.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View, AppRegistry } from 'react-native';
+
+const App = () => {
+    return (
+        <View>
+            <Text>Moola</Text>
+        </View>
+    );
+};
+
+AppRegistry.registerComponent('Moola', () => App);
